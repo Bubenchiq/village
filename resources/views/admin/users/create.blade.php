22425dfc7f3583f4ad5aac40f1c7a001045a6bbe
Vladimir <69737120+plasticground@users.noreply.github.com>
2022-09-21 22:00:43 +0300

    Update create.blade.php

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -35,7 +35,7 @@
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
-                    <input type="password" name="password" placeholder="User's name" id="password" class="form-control">
+                    <input type="password" name="password" placeholder="User's password" id="password" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
